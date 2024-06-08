6790bedd1c58f0f0f29efcfee16d3238b82700a1
OlTaras <oliinyk.taras@gmail.com>
2024-06-08 12:35:45 -0500

    planet2 is done

--- src/test/java/SolarSystem/SolarSystem.java
+++ src/test/java/SolarSystem/SolarSystem.java
@@ -8,10 +8,21 @@ public class SolarSystem {
         solar.planet="2";
         solar.star="2";
         System.out.println("The solar system is ready ");
+
+
+        // ...
         FeatureSun xros=new FeatureSun();
         xros.color="pink";
         xros.temp="23424Kv";
         xros.size="OKm";
+
+
+        // ...
+        Planet2 perox = new Planet2();
+        perox.color="green";
+        perox.size="1m";
+        perox.radius="3m";
+        
     }
 
 }
