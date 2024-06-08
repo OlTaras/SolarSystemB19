291018b674506ba3c1fd5987b9e13186ca378834
OlTaras <oliinyk.taras@gmail.com>
2024-06-08 12:12:34 -0500

    done with planet1

--- src/test/java/SolarSystem/SolarSystem.java
+++ src/test/java/SolarSystem/SolarSystem.java
@@ -8,10 +8,21 @@ public class SolarSystem {
         solar.planet="2";
         solar.star="2";
         System.out.println("The solar system is ready ");
+
+
+        // ...
         FeatureSun xros=new FeatureSun();
         xros.color="pink";
         xros.temp="23424Kv";
         xros.size="OKm";
+
+
+        // ...
+        Planet1 renox = new Planet1();
+        renox.color="gray";
+        renox.size="10km";
+        renox.radius="5m";
+        
     }
 
 }
